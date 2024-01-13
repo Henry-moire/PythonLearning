cc6ddd8072a4d8a81d6ba7b46cf1c7122e778a04
Nyakundi <henrymoire@gmail.com>
2024-01-13 09:51:39 +0300

    Finished basic operations tutorial

---
+++ basicoperations.py
@@ -0,0 +1,20 @@
+number = 1 + 2 * 3 / 4.0
+print(number)
+# Using two multiplication symbols makes a power relationship.
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+# Python supports concatenating strings using the addition operator:
+helloworld = "hello" + " " + "world"
+print(helloworld)
+# Python also supports multiplying strings to form a string with a repeating sequence:
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
+# Lists can be joined with the addition operators:
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+# Just as in strings, Python supports forming new lists with a repeating sequence using the multiplication operator:
+print([1,2,3] * 3)
